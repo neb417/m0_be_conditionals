f2629878ee844f84f6c2742064e97dfd7a3954f7
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-06-08 14:31:47 -0600

    Initial commit

---
+++ code_challenges/mild.rb
@@ -0,0 +1,11 @@
+#Write a Ruby program that defines a variable that stores an Integer.
+#The program should print out the String "even" if the Integer is even,
+#and the String "odd" if the Integer is odd.
+
+var = 2
+
+if var.even?
+  p "even"
+else
+  p "odd"
+end
